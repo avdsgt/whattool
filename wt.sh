c75096c2622b4f5729602f12b73e11796c45f6c4
AVADHESH GUPTA <avdsgt@gmail.com>
2020-12-27 18:05:41 -0500

    Inserted Case Statment
    Here user insert tool option provided in given list

--- wt.sh
+++ wt.sh
@@ -1,1 +1,69 @@
 
+echo " 
+
+#!/bin/bash 
+
+###################################################################################################################################################################
+#Script Name	: WhatTool   
+#Version	: 1.0                                                                    
+#Description	: Full Descriptio About All Penetesting Tools . This tool will help you to choose which SecTools is better for you and also provide downloading link.                                                          
+#Author       	: Avadhesh Gupta                                
+#Email         	: avdsgt@gmail.com  
+#Twitter        : https://twitter.com/avdsgt
+#Github         : https://github.com/avdsgt
+####################################################################################################################################################################"
+
+echo "  
+ _            _              ______         _  
+(_|   |   |_/| |            (_) |          | | 
+  |   |   |  | |     __, _|_    | __   __  | | 
+  |   |   |  |/ \   /  |  |   _ |/  \_/  \_|/  
+   \_/ \_/   |   |_/\_/|_/|_/(_/ \__/ \__/ |__/"
+
+echo "
+
+
+"
+
+
+
+echo " Select What Kind Of Tool You Want To Use
+
+	1. Information Gathering
+	2. Vulnerability Analysis
+	3. Web Application Analysis
+	4. Database Assessment 
+	5. Password Attack 
+	6. Reverse Engineering
+	7. Exploitation Tools
+	8. Sniffing And Spoofing
+	9. Post Exploitation 
+	10.Forensics Tools
+	11.Reporting Tool
+	12.Social Engineering Tools"
+
+read input
+
+case $input in 
+	1)
+	echo "Ypu CHoos Information F"
+	;;
+	2)
+	echo "Vulnerability"
+	;;
+esac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
